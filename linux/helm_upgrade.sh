c3a4e01a907f6bba388427be1f04f1a1ed6fa66f
jaska <pusanteemu@gmail.com>
2018-03-04 20:06:29 +0200

    Lisätty etuliite release nimeen.

--- linux/helm_upgrade.sh
+++ linux/helm_upgrade.sh
@@ -3,6 +3,6 @@ IMAGE_TAG="$CI_COMMIT_REF_NAME-$CI_COMMIT_SHA"
 RELEASE_NAME=$(echo "$CI_COMMIT_REF_NAME" | sed 's/\./-/g')
 
 helm upgrade \
-	--wait \
-	--set petriImage=jaska/petri:$IMAGE_TAG \
-	--install $RELEASE_NAME ./deployment
+--wait \
+--set petriImage=jaska/petri:$IMAGE_TAG \
+--install eptri-$RELEASE_NAME ./deployment
