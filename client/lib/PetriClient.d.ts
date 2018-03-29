82b46d8fac164d0bba1acef59090616309c5baca
jaska <pusanteemu@gmail.com>
2018-03-29 23:01:29 +0300

    Poistettu lib kansio

--- client/lib/PetriClient.d.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient } from "apollo-client";
-import { NormalizedCacheObject } from "apollo-cache-inmemory";
-export declare const createPetriClient: (args: {
-    graphqlHost: string;
-    graphqlPort: number;
-    graphqlSubscriptionsHost: string;
-    graphqlSubscriptionsPort: number;
-}) => ApolloClient<NormalizedCacheObject>;
