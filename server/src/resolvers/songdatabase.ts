f0bf43c8229d48f69239f0590fec2feafdd81699
jaska <pusanteemu@gmail.com>
2018-04-18 22:49:14 +0300

    Lisätty layulutietokantojen laulujen haku ja niiden lisäys ja poistaminen.

--- server/src/resolvers/songdatabase.ts
+++ server/src/resolvers/songdatabase.ts
@@ -1,5 +1,10 @@
 import { Context } from "../context";
-import { SongDatabaseQueryArgs } from "../schemadef";
+import {
+	SongDatabaseQueryArgs,
+	SongDatabaseVariationsQueryArgs,
+	AddVariationToSongDatabaseMutationArgs,
+	RemoveVariationFromSongDatabaseMutationArgs
+} from "../schemadef";
 
 export const Query = {
 	songDatabase: async (
@@ -20,5 +25,55 @@ export const Query = {
 			totalCount: res.maxSongDatabases,
 			songDatabases: res.songDatabases
 		};
+	},
+	songDatabaseVariations: async (
+		root,
+		args: SongDatabaseVariationsQueryArgs,
+		context: Context
+	) => {
+		const songDatabaseId = parseInt(args.songDatabaseId, 10);
+
+		const res = await context.seppo.searchVariations({
+			songDatabaseId,
+			limit: 50
+		});
+
+		return {
+			totalCount: res.maxVariations,
+			variations: res.variations
+		};
+	}
+};
+
+export const Mutation = {
+	addVariationToSongDatabase: async (
+		root,
+		args: AddVariationToSongDatabaseMutationArgs,
+		context: Context
+	) => {
+		const songDatabaseId = parseInt(args.songDatabaseId, 10);
+		const variationId = parseInt(args.variationId, 10);
+
+		const res = await context.seppo.updateVariation({
+			variationId: variationId,
+			addSongDatabaseIds: [songDatabaseId]
+		});
+
+		return res.success;
+	},
+	removeVariationFromSongDatabase: async (
+		root,
+		args: RemoveVariationFromSongDatabaseMutationArgs,
+		context: Context
+	) => {
+		const songDatabaseId = parseInt(args.songDatabaseId, 10);
+		const variationId = parseInt(args.variationId, 10);
+
+		const res = await context.seppo.updateVariation({
+			variationId: variationId,
+			removeSongDatabaseIds: [songDatabaseId]
+		});
+
+		return res.success;
 	}
 };
